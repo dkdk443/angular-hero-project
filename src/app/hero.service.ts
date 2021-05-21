ea8d73df0e6b8fe78fa69da251d972e930e0f1f3
dkdk443 <demo_weeeds_470@yahoo.co.jp>
2021-05-21 22:53:20 +0900

    ヒーロー一覧取得メッセージの知っ蔵

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { Hero } from './hero';
 import { HEROES } from './mock-heros';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +10,10 @@ import { HEROES } from './mock-heros';
 export class HeroService {
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
+    this.messageService.add('ヒーロー一覧を取得しました！');
     return heroes;
   }
 
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 }
